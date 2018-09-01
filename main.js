26b888e541714c06187795dfbb38f46422af738d
anderson sanchez <andersonsanchez53@gmail.com>
2018-08-31 20:00:32 -0700

    Add connection request and response funcs

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 'use strict'
 const fs = require('fs');
 const bencode = require('bencode');
+const tracker = require('./tracker');
 
 const torrent = bencode.decode(fs.readFileSync('Keijo 2016 SDM Dual Audio HEVC 10 bit.torrent'));
-console.log(torrent.announce.toString('utf8'));
+
+tracker.getPeers(torrent, peers => {
+    console.log('list of peers: ', peers);
+});
