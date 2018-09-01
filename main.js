901a9822a056a087a0a4bb1d30e2ea815fe34eb6
anderson sanchez <andersonsanchez53@gmail.com>
2018-08-31 19:33:45 -0700

    Add required packages

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-var a = 4;
+'use strict'
+const fs = require(fs);
+const bencode = require('bencode');
+
+const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
+console.log(torrent.announce.toString('utf8'));
